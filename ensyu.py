46d743f4838ea5e33a4f534f1de2c1c9d7420178
Max <shidoutsuruya@gmail.com>
2025-03-04 21:33:44 +0800

    tere

--- ensyu.py
+++ ensyu.py
@@ -14,7 +14,7 @@ class decide:
     def __init__(self,img_path:str):
         self.img=(plt.imread(img_path)*255).astype(np.uint8)
         self.img_path=img_path
-        self.close=True
+        self.close=False
     def image_catch(self):
         screenshot=pyautogui.screenshot()
         screenshot.save(self.img_path)
@@ -50,19 +50,18 @@ class decide:
             contours,_=cv2.findContours(closed,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
             rects=[]
             for c in contours:
-                if cv2.contourArea(c)>20000 and cv2.contourArea(c)<45000:
+                if cv2.contourArea(c)>10000 and cv2.contourArea(c)<20000:
                     epsilon=0.05*cv2.arcLength(c,True)
                     approx=cv2.approxPolyDP(c,epsilon,True)
                     if len(approx)==6:
                         cv2.drawContours(image,[approx],0,(255,0,0),10)
                         rects.append(approx)
-            #plt.imshow(image)
-            #plt.show()
-            centroid=rects[0].mean(axis=0)
-            centroid=tuple(centroid.flatten().astype(int))
-            pyautogui.doubleClick(centroid[0],centroid[1])
+            rects=np.array(rects)
+            centroid=rects.mean(axis=1)
+            centroid=centroid.mean(axis=0).astype(int).flatten()
+            pyautogui.doubleClick(centroid[0],centroid[1]+100)
             cv2.circle(image,centroid,5,(0,0,255),5)
-            
+            #sys.exit()
     def find_new_katana(self):
         position=([[1492,793],[1531,791],[1542,813]])
         if self.position_color_check(position,KATANA_NAVY)==3:
@@ -80,11 +79,24 @@ class decide:
                 #computer will close
                 os.system("shutdown /s /t 200")
             sys.exit()
+    def alert_check(self):
+        self.image_catch()
+        red_position=([714,658],[733,658])
+        if self.position_color_check(red_position,ALERT_RED)==2:
+            print("alert detected")
+            if self.close:
+                #close chrome
+                os.system("taskkill /im chrome.exe /f")
+                time.sleep(5)
+                #computer will close
+                os.system("shutdown /s /t 200")
+            sys.exit()
 if __name__=="__main__":
     while True:
         d=decide(FOLDER_PATH+"/screenshot.png")
         d.continue_run()
         d.find_new_katana()
         d.tired_check()
+        d.alert_check()
         d.normal_click()
         time.sleep(1)
